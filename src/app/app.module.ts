7d6feb567500ef550305da96e033a29d53dd7714
prasad <prasadhaval.india@gmail.com>
2024-12-21 12:56:45 +0530

    add configration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { DashbordComponent } from './component/dashbord/dashbord.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashbordComponent
   ],
   imports: [
     BrowserModule,
